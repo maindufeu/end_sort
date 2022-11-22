bddf841936f9f55f4289c9d5d3841c1293240506
Edher Jiménez <edher.jimenez@gmail.com>
2022-11-21 22:50:15 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,17 +79,14 @@ else:
     
 st.graphviz_chart('''
     digraph {
-        run -> intr intr -> runbl
-        runbl -> run
-        run -> kernel
-        kernel -> zombie
-        kernel -> sleep
-        kernel -> runmem
-        sleep -> swap
-        swap -> runswap
-        runswap -> new
-        runswap -> runmem
-        new -> runmem
-        sleep -> runmem
+        run -> intr intr -> runbl runbl -> run run -> kernel kernel -> zombie
+kernel -> sleep
+kernel -> runmem
+sleep -> swap
+swap -> runswap
+runswap -> new
+runswap -> runmem
+new -> runmem
+sleep -> runmem
     }
 ''')
